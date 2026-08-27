b93c74593aa28fcdd18c904afbbc954e2dbb8242
kevvy555 <kevvy555@googlemail.com>
2026-08-27 22:58:12 +0100

    Add Life Spark bonus targets

---
+++ src/systems/SparkSystem.js
@@ -0,0 +1,58 @@
+import { GAME_CONFIG, ENTITY_DEFINITIONS } from '../core/config.js';
+
+export class SparkSystem {
+  constructor(world, mergeSystem, random = Math.random) {
+    this.world = world;
+    this.mergeSystem = mergeSystem;
+    this.random = random;
+  }
+
+  consumeAt(x, y) {
+    const spark = this.world.activeSpark;
+    if (!spark || spark.x !== x || spark.y !== y) return null;
+
+    const spawnLevel = GAME_CONFIG.spark.spawnLevel;
+    const definition = ENTITY_DEFINITIONS[spawnLevel];
+    const wasNewDiscovery = spawnLevel > this.world.discoveredLevel;
+
+    this.world.activeSpark = null;
+    this.world.sparksCollected += 1;
+    this.world.nextSparkAt = this.world.taps + GAME_CONFIG.spark.interval;
+    this.world.setCell(x, y, { level: spawnLevel });
+    this.world.addScore(GAME_CONFIG.spark.score + definition.score);
+
+    const events = [
+      { type: 'spawn', x, y, level: spawnLevel },
+      { type: 'sparkCollected', x, y, bonus: GAME_CONFIG.spark.score },
+    ];
+
+    if (wasNewDiscovery) {
+      this.world.discover(spawnLevel);
+      this.world.addScore(definition.discoveryBonus);
+      events.push({ type: 'discovery', x, y, level: spawnLevel, bonus: definition.discoveryBonus });
+    }
+
+    events.push(...this.mergeSystem.resolveFrom(x, y));
+    return events;
+  }
+
+  maybeSpawn() {
+    if (this.world.activeSpark || this.world.taps < this.world.nextSparkAt) return [];
+
+    const emptyCells = [];
+    for (let y = 0; y < this.world.rows; y += 1) {
+      for (let x = 0; x < this.world.columns; x += 1) {
+        if (!this.world.getCell(x, y)) emptyCells.push({ x, y });
+      }
+    }
+
+    if (emptyCells.length === 0) {
+      this.world.nextSparkAt = this.world.taps + 3;
+      return [];
+    }
+
+    const index = Math.min(emptyCells.length - 1, Math.floor(this.random() * emptyCells.length));
+    this.world.activeSpark = emptyCells[index];
+    return [{ type: 'sparkAppeared', ...this.world.activeSpark }];
+  }
+}
